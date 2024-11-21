440bcada8121e8b8e58f976d5d6490f18a0a4f25
Enrico Guiraud <eguiraud@proximafusion.com>
2024-11-21 20:59:59 +0100

    Import fix-ups to make vmecpp/simsopt_compat work in this repo

--- src/vmecpp/cpp/vmecpp/simsopt_compat/__init__.py
+++ src/vmecpp/cpp/vmecpp/simsopt_compat/__init__.py
@@ -1,13 +1,13 @@
-from vmecpp.simsopt_compat._fortran_wout_adapter import (
+from vmecpp.cpp.vmecpp.simsopt_compat._fortran_wout_adapter import (
     VARIABLES_MISSING_FROM_FORTRAN_WOUT_ADAPTER,
     FortranWOutAdapter,
 )
-from vmecpp.simsopt_compat._indata_to_surfacerzfourier import (
+from vmecpp.cpp.vmecpp.simsopt_compat._indata_to_surfacerzfourier import (
     surfacerzfourier_from_any_vmec_indata,
     surfacerzfourier_from_fourier_coeffs,
     surfacerzfourier_from_vmecppindata,
 )
-from vmecpp.simsopt_compat._vmec_indata_compat import (
+from vmecpp.cpp.vmecpp.simsopt_compat._vmec_indata_compat import (
     ensure_vmec2000_input,
     ensure_vmecpp_input,
     is_vmec2000_input,
