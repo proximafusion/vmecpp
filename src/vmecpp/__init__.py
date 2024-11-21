fa117502b043d07e4867e489c94ee5995702d593
Enrico Guiraud <eguiraud@proximafusion.com>
2024-11-21 16:08:36 +0100

    Fix copyright headers

--- src/vmecpp/__init__.py
+++ src/vmecpp/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-present Proxima Fusion Gmbh <info@proximafusion.com>
+# SPDX-FileCopyrightText: 2024-present Proxima Fusion GmbH <info@proximafusion.com>
 #
 # SPDX-License-Identifier: MIT
 from vmecpp._vmecpp import run
