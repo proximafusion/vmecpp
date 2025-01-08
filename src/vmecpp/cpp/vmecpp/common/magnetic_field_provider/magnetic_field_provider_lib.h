4311e6bbc7c93e789758c320c99956ead88d523a
Jonathan Schilling <jons@proximafusion.com>
2025-01-08 14:27:00 +0100

    Migrate the CurveRZFourier protobuf type to a C++ struct.

--- src/vmecpp/cpp/vmecpp/common/magnetic_field_provider/magnetic_field_provider_lib.h
+++ src/vmecpp/cpp/vmecpp/common/magnetic_field_provider/magnetic_field_provider_lib.h
@@ -5,6 +5,7 @@
 
 #include "absl/status/status.h"
 #include "vmecpp/common/composed_types_definition/composed_types.pb.h"
+#include "vmecpp/common/composed_types_definition/composed_types.h"
 #include "vmecpp/common/magnetic_configuration_definition/magnetic_configuration.h"
 
 namespace magnetics {
