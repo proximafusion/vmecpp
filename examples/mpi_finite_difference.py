e86d3e5bc279b4dc4db3f9015c2504376c65eaae
Jonathan Schilling <jons@proximafusion.com>
2025-03-13 08:17:34 +0100

    Make a backup of the original input in gradient approx ...
    ... in order to not have finite-difference steps accumulate.

--- examples/mpi_finite_difference.py
+++ examples/mpi_finite_difference.py
@@ -10,38 +10,41 @@ rank = comm.Get_rank()
 
 # In a real application, only root would probably read the input file and broadcast it.
 filename = Path(__file__).parent / "data" / "w7x.json"
-input = vmecpp.VmecInput.from_file(filename)
+initial_input = vmecpp.VmecInput.from_file(filename)
 
-n_dofs = np.prod(input.rbc.shape) + np.prod(input.zbs.shape)
+n_dofs = np.prod(initial_input.rbc.shape) + np.prod(initial_input.zbs.shape)
 m_outputs = 1  # Only interested in the volume
 # One process per DOF. Root can also do a finite difference evaluation
 assert n_dofs % comm.Get_size() == 0, f"Number of degrees of freedom: {n_dofs}"
 n_dofs_per_proc = n_dofs // comm.Get_size()
 
 # Base evaluation
-output = None
+initial_output = None
 if rank == 0:
-    output = vmecpp.run(input)
-output = comm.bcast(output)
+    initial_output = vmecpp.run(initial_input)
+initial_output = comm.bcast(initial_output)
 
 # ...and fix up the multigrid steps: hot-restarted runs only allow a single step
-input.ns_array = input.ns_array[-1:]
-input.ftol_array = input.ftol_array[-1:]
-input.niter_array = input.niter_array[-1:]
+initial_input.ns_array = initial_input.ns_array[-1:]
+initial_input.ftol_array = initial_input.ftol_array[-1:]
+initial_input.niter_array = initial_input.niter_array[-1:]
 
 eps = 1e-8
 my_jacobian = np.zeros((n_dofs_per_proc, m_outputs))
 # Start the finite difference evaluation
 for i in range(n_dofs_per_proc):
+    perturbed_input = initial_input.model_copy(deep=True)
     dof_idx = i + rank * n_dofs_per_proc
     if dof_idx < n_dofs // 2:
-        input.rbc.flat[dof_idx] += eps
+        perturbed_input.rbc.flat[dof_idx] += eps
     else:
-        input.zbs.flat[dof_idx - n_dofs // 2] += eps
+        perturbed_input.zbs.flat[dof_idx - n_dofs // 2] += eps
 
     # We can now run a finite difference evaluation with hot restart:
-    hot_restarted_output = vmecpp.run(input, restart_from=output, verbose=False)
-    dVdx = (hot_restarted_output.wout.volume - output.wout.volume) / eps
+    hot_restarted_output = vmecpp.run(
+        perturbed_input, restart_from=initial_output, max_threads=1
+    )
+    dVdx = (hot_restarted_output.wout.volume - initial_output.wout.volume) / eps
     print(f"{dof_idx:3d} dVdx: {dVdx}")
     my_jacobian[i, :] = dVdx
 
