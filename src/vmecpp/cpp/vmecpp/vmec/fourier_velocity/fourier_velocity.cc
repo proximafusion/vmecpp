80f1ade9b830417e54e53ffb5a8596fcefa5e1ca
jungdaesuh <78460559+jungdaesuh@users.noreply.github.com>
2026-02-11 21:24:14 +0900

    Fix #164: rebind spans after copy/move/assignment (#400)
    * Fix #164: rebind spans after copy/move/assignment
    
    * Fix CI: simsopt resize return + move warnings
    
    * Update simsopt_compat.py
    
    ---------
    
    Co-authored-by: Philipp Jurašić <166746189+jurasic-pf@users.noreply.github.com>

--- src/vmecpp/cpp/vmecpp/vmec/fourier_velocity/fourier_velocity.cc
+++ src/vmecpp/cpp/vmecpp/vmec/fourier_velocity/fourier_velocity.cc
@@ -4,6 +4,8 @@
 // SPDX-License-Identifier: MIT
 #include "vmecpp/vmec/fourier_velocity/fourier_velocity.h"
 
+#include <utility>
+
 namespace vmecpp {
 
 FourierVelocity::FourierVelocity(const Sizes* s, const RadialPartitioning* r,
@@ -21,5 +23,66 @@ FourierVelocity::FourierVelocity(const Sizes* s, const RadialPartitioning* r,
       vlcs(lcs),
       vlcc(lcc),
       vlss(lss) {}
+
+FourierVelocity::FourierVelocity(const FourierVelocity& other)
+    : FourierCoeffs(other),
+      vrcc(rcc),
+      vrss(rss),
+      vrsc(rsc),
+      vrcs(rcs),
+      vzsc(zsc),
+      vzcs(zcs),
+      vzcc(zcc),
+      vzss(zss),
+      vlsc(lsc),
+      vlcs(lcs),
+      vlcc(lcc),
+      vlss(lss) {}
+
+void FourierVelocity::BindSpans() {
+  vrcc = rcc;
+  vrss = rss;
+  vrsc = rsc;
+  vrcs = rcs;
+  vzsc = zsc;
+  vzcs = zcs;
+  vzcc = zcc;
+  vzss = zss;
+  vlsc = lsc;
+  vlcs = lcs;
+  vlcc = lcc;
+  vlss = lss;
+}
+
+FourierVelocity& FourierVelocity::operator=(const FourierVelocity& other) {
+  if (this != &other) {
+    FourierCoeffs::operator=(other);
+    BindSpans();
+  }
+  return *this;
+}
+
+FourierVelocity::FourierVelocity(FourierVelocity&& other) noexcept
+    : FourierCoeffs(std::move(other)),
+      vrcc(rcc),
+      vrss(rss),
+      vrsc(rsc),
+      vrcs(rcs),
+      vzsc(zsc),
+      vzcs(zcs),
+      vzcc(zcc),
+      vzss(zss),
+      vlsc(lsc),
+      vlcs(lcs),
+      vlcc(lcc),
+      vlss(lss) {}
+
+FourierVelocity& FourierVelocity::operator=(FourierVelocity&& other) noexcept {
+  if (this != &other) {
+    FourierCoeffs::operator=(std::move(other));
+    BindSpans();
+  }
+  return *this;
+}
 
 }  // namespace vmecpp
