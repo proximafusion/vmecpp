5a9f02be375c58151d0ff7dfbb7716a1545ffb66
EdoAlvarezR <edo.alvarezr@gmail.com>
2026-04-07 21:28:26 -0500

    Automatic changes by pre-commit run --all-files

--- examples/visualize_magnetic_field.py
+++ examples/visualize_magnetic_field.py
@@ -1,15 +1,15 @@
-"""Showcases how to (1) postprocess VMEC++ outputs to reconstruct the magnetic 
-field from Fourier coefficients, (2) transform field from toroidal to 
-cylindrical and Cartesian coordinates, and (3) visualize flux surface + magnetic 
-field using pyVista.
+"""Showcases how to (1) postprocess VMEC++ outputs to reconstruct the magnetic field
+from Fourier coefficients, (2) transform field from toroidal to cylindrical and
+Cartesian coordinates, and (3) visualize flux surface + magnetic field using pyVista.
 
 Note, that this script requires pyvista as an additional dependency.
 """
 
-def calculate_magnetic_field(vmec_output, j, theta, phi, 
-                                output_coordinates='cartesian'):
-    """
-    Evaluate magnetic field at a toroidal point described by `(j, theta, phi)`.
+
+def calculate_magnetic_field(
+    vmec_output, j, theta, phi, output_coordinates="cartesian"
+):
+    """Evaluate magnetic field at a toroidal point described by `(j, theta, phi)`.
 
     Reference: J. Schilling (2026), "The Numerics of VMEC++", Proxima Fusion.
 
@@ -19,7 +19,7 @@ def calculate_magnetic_field(vmec_output, j, theta, phi,
         VMEC++ output from `vmecpp.run(...)`.
     j : int
         Index of the flux surface (between 0 and `vmec_output.wout.ns - 1`,
-        where 0 is the magnetic axis and `vmec_output.wout.ns - 1` is the 
+        where 0 is the magnetic axis and `vmec_output.wout.ns - 1` is the
         outermost flux surface).
     theta : float
         Polidal angle between `0` and `2*pi`.
@@ -31,28 +31,28 @@ def calculate_magnetic_field(vmec_output, j, theta, phi,
     Returns
     -------
     ndarray, ndarray, ndarray
-        Position `X`, magnetic field vector `B`, and current density `J` in the 
+        Position `X`, magnetic field vector `B`, and current density `J` in the
         desired coordinates.
     """
-    
+
     # Fetch poloidal mode numbers: m for position, m_nyq for magnetic field
     xm = vmec_output.wout.xm
     xm_nyq = vmec_output.wout.xm_nyq
-    
+
     # Fetch toroidal mode numbers: n * nfp for position, n_nyq * nfp for magnetic field
     xn = vmec_output.wout.xn
     xn_nyq = vmec_output.wout.xn_nyq
-    
+
     # Fetch position Fourier coefficients
     rmnc = vmec_output.wout.rmnc
     zmns = vmec_output.wout.zmns
-    
+
     # Fetch B^θ_mn and B^ζ_mn cos Fourier coefficients
-    # NOTE: From Schilling 2026 VMEC++ white paper, Table 9.2, we have 
+    # NOTE: From Schilling 2026 VMEC++ white paper, Table 9.2, we have
     #       B^{θ,cos}_mn = bsupumnc and B^{ζ,cos}_mn = bsupvmnc
     bsupumnc = vmec_output.wout.bsupumnc
     bsupvmnc = vmec_output.wout.bsupvmnc
-    
+
     # Arguments of Fourier series
     kernel = xm * theta - xn * phi
     kernel_nyq = xm_nyq * theta - xn_nyq * phi
@@ -61,25 +61,25 @@ def calculate_magnetic_field(vmec_output, j, theta, phi,
     cosk = np.cos(kernel)
     sink = np.sin(kernel)
     cosk_nyq = np.cos(kernel_nyq)
-    
+
     # Position in cylindrical coordinates (Sec. 9.1)
     r = np.dot(rmnc[:, j], cosk)
     z = np.dot(zmns[:, j], sink)
-    
+
     # Cylindrical position derivatives (Sec. 3.5)
     drdtheta = np.dot(rmnc[:, j], -xm * sink)
     drdphi = np.dot(rmnc[:, j], xn * sink)
     dzdtheta = np.dot(zmns[:, j], xm * cosk)
     dzdphi = np.dot(zmns[:, j], -xn * cosk)
-    
+
     # Position in Cartesian coordinates (Sec. 3.7)
     x = r * np.cos(phi)
     y = r * np.sin(phi)
-    
+
     # Construct contravariant components B^θ and B^ζ from Fourier series (Sec. 9.2)
     bsupu = np.dot(bsupumnc[:, j], cosk_nyq)
     bsupv = np.dot(bsupvmnc[:, j], cosk_nyq)
-    
+
     # Construct cylindrical components from contraviariant components
     br = bsupu * drdtheta + bsupv * drdphi
     bphi = bsupv * r
@@ -96,15 +96,13 @@ def calculate_magnetic_field(vmec_output, j, theta, phi,
     # currphi = currv * r
     # currz = curru * dzdtheta + currv * dzdphi
 
-    if output_coordinates == 'cylindrical':
-
+    if output_coordinates == "cylindrical":
         # Format outputs as vectors
-        X = np.array([r, phi, z])                   # Position in cylindrical coordinates
-        B = np.array([br, bphi, bz])                # Magnetic field in cylindrical coordinates
+        X = np.array([r, phi, z])  # Position in cylindrical coordinates
+        B = np.array([br, bphi, bz])  # Magnetic field in cylindrical coordinates
         # J = np.array([currr, currphi, currz])     # Current density in cylindrical coordinates
 
-    elif output_coordinates == 'cartesian':
-    
+    elif output_coordinates == "cartesian":
         # Magnetic field in Cartesian coordinates (Sec. 3.7)
         bx = br * np.cos(phi) - bphi * np.sin(phi)
         by = br * np.sin(phi) + bphi * np.cos(phi)
@@ -113,30 +111,27 @@ def calculate_magnetic_field(vmec_output, j, theta, phi,
         # curry = currr * np.sin(phi) + currphi * np.cos(phi)
 
         # Format outputs as vectors
-        X = np.array([x, y, z])                     # Position in Cartesian coordinates
-        B = np.array([bx, by, bz])                  # Magnetic field in Cartesian coordinates
+        X = np.array([x, y, z])  # Position in Cartesian coordinates
+        B = np.array([bx, by, bz])  # Magnetic field in Cartesian coordinates
         # J = np.array([currx, curry, currz])       # Current density in Cartesian coordinates
 
-    
     else:
-        raise ValueError(f"Invalid `output_coordinates = {output_coordinates}`;"
-                          + " valid values are 'cartesian' and 'cylindrical'.")
-    
+        raise ValueError(
+            f"Invalid `output_coordinates = {output_coordinates}`;"
+            + " valid values are 'cartesian' and 'cylindrical'."
+        )
+
     # return X, B, J
     return X, B
 
 
-
-
 if __name__ == "__main__":
-
     from pathlib import Path
-    import numpy as np
 
+    import numpy as np
     import pyvista as pv
 
     import vmecpp
-
 
     # ----------- RUN VMEC++ ---------------------------------------------------
 
@@ -146,7 +141,6 @@ if __name__ == "__main__":
 
     # Run VMEC++:
     vmec_output = vmecpp.run(vmec_input)
-
 
     # ----------- CONSTRUCT MAGNETIC FIELD OVER A FLUX SURFACE -----------------
 
@@ -161,8 +155,8 @@ if __name__ == "__main__":
     num_phi = 181
 
     # Grid in theta and phi along the flux surface
-    grid_theta = np.linspace(0.0, 2*np.pi, num_theta, endpoint=True)
-    grid_phi = np.linspace(0.0, 2*np.pi, num_phi, endpoint=True)
+    grid_theta = np.linspace(0.0, 2 * np.pi, num_theta, endpoint=True)
+    grid_phi = np.linspace(0.0, 2 * np.pi, num_phi, endpoint=True)
 
     # Compute Cartesian coordinates of flux surface geometry and magnetic field
     x, y, z = (np.zeros([num_theta, num_phi]) for i in range(3))
@@ -171,7 +165,6 @@ if __name__ == "__main__":
 
     for idx_theta, theta in enumerate(grid_theta):
         for idx_phi, phi in enumerate(grid_phi):
-
             # Compute
             # X, B, J = calculate_magnetic_field(vmec_output, j, theta, phi)
             X, B = calculate_magnetic_field(vmec_output, j, theta, phi)
@@ -189,7 +182,6 @@ if __name__ == "__main__":
             # jy[idx_theta, idx_phi] = J[1]
             # jz[idx_theta, idx_phi] = J[2]
 
-
     # ----------- VISUALIZE WITH pyVista ---------------------------------------
 
     # Create structured grid
@@ -200,8 +192,8 @@ if __name__ == "__main__":
     J = np.stack((jx, jy, jz), axis=2)
 
     # Reshape to (n_points, 3)
-    B = B.reshape(-1, 3, order='F')
-    J = J.reshape(-1, 3, order='F')
+    B = B.reshape(-1, 3, order="F")
+    J = J.reshape(-1, 3, order="F")
 
     # Attach vector field
     grid["B"] = B
@@ -223,11 +215,7 @@ if __name__ == "__main__":
     )
 
     # Add magnetic field glyphs
-    glyphs = grid.glyph(
-        orient="B",
-        scale="B",
-        factor=0.03
-    )
+    glyphs = grid.glyph(orient="B", scale="B", factor=0.03)
     plotter.add_mesh(glyphs, color="blue")
 
     # Visualize
